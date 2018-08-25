f27c5e98c514503c0ae8c2c6eb73e9cbb35fa21b
Krakaw <ks@tari.com>
2018-08-25 08:12:25 -0700

    Enhancements
    Closes #2 #3 #4 #5 #6

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,41 @@
 <template>
-  <div id="app" class="md-app">
-    <Issues msg="Welcome to Your Vue.js App"/>
-  </div>
+  <md-app id="app" class="md-app">
+    <md-app-content>
+      <Issues />
+      <span class="version">{{version}}</span>
+    </md-app-content>
+
+  </md-app>
 </template>
 
 <script>
-import Issues from './components/Issues.vue'
-
+import Issues from "./components/Issues.vue";
+import { version } from "../package.json";
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Issues
+  },
+  computed: {
+    version() {
+      return version;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.version {
+  position: fixed;
+  bottom: 0;
+  right: 0;
 }
 </style>
